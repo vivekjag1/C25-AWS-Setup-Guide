867748c5d62f5d622f35dc786b076de6f1abca38
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2025-01-26 22:35:06 -0500

    remove code

--- app/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-auth_blueprint = Blueprint('auth', __name__)
-
-from app.auth import auth_routes
-
--- app/errors/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-error_blueprint = Blueprint('errors', __name__)
-
-from app.errors import handlers
